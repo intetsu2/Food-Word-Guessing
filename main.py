14362498e00e1277dace10920620f006ab4dd4c7
Paul Ndaboreheye <86745375+intetsu2@users.noreply.github.com>
2022-07-05 19:46:04 -0600

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random as rand
 import food as fd
-import os 
-import sys
+import time
 
-# Random Fruits function
-def random_food(f_v):
+# Random Fruits & Vegetables function
+def random_food(f_or_v, option_check):
     point_count = 0
     time_play = 0
     missed_guesses = 0
@@ -12,22 +11,31 @@ def random_food(f_v):
 
     while time_play != 6:
         time_play += 1
-        rdf_c = rand.choice(f_v)
-        print(f'Guess a fruit or a vegetable that starts with {rdf_c[:2]}')
+        rdf_c = rand.choice(f_or_v)
+        if option_check == 'Fruits':
+            print(f'Guess a fruit that starts with {rdf_c[:2]}')
+        elif option_check == 'Vegetables':
+            print(f'Guess a vegetable that starts with {rdf_c[:2]}')
 
         # fruit value input control
         try:
-            guess = str(input('Guess: '))
+            time.sleep(2)
+            guess = str(input('Guess or Quit: ')).capitalize()
             if guess == rdf_c:
                 print("Correct!")
                 point_count += 1
                 guessesd_guesses += 1
                 print('+1 point')
             elif guess != rdf_c:
+                if guess == 'Quit':
+                    break
+                else:
+                    continue
                 point_count -= 1
                 missed_guesses += 1
                 print("Wrong guess!")
                 print('-1 point')
+
         except ValueError:
             print('Invalid input!')
 
@@ -46,17 +54,21 @@ def main():
     fruits = fd.all_food['fruits']
     games = ['Fruits', 'Vegetables']
     print(f'Game choices are {games[0]} and {games[1]} or Exit')
+
     while True:
-        game_choice = input("Choose game: ")
+        game_choice = input("Choose game: ").capitalize()
         if game_choice in games:
             if game_choice == games[0]:
-                random_food(fruits)
+                random_food(fruits, game_choice)
             elif game_choice == games[1]:
-                random_food(vegetables)
+                random_food(vegetables, game_choice)
         else:
             if games == "Exit":
                 break
             else:
+                print('Checking your input....')
+                time.sleep(4)
                 print('Invalid choice, try agai!')
 
-main()
+if __name__ == "__main__":
+    main()
